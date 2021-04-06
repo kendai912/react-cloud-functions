89075a188b8414c3ab42be83e28f2a508f58abeb
Kendai <kendai912@gmail.com>
2021-04-06 09:29:05 +0900

    restructured api functions

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,8 @@
 const functions = require("firebase-functions");
 const express = require("express");
-const requestPromise = require("request-promise-native");
-const cors = require("cors");
 const axios = require("axios").default;
+// const requestPromise = require("request-promise-native");
+const cors = require("cors");
 
 // local
 // http://localhost:5000/react-cloud-functions-b412b/us-central1/
@@ -11,14 +11,53 @@ const axios = require("axios").default;
 
 const app = express();
 
+// 全てのapiリクエストにcorsを設定する場合
 // app.use(cors());
 
 // APIにリクエストを送る関数を定義
-const getDataFromApi = async (keyword) => {
+const getDataFromBookApi = async (keyword) => {
   // cloud functionsから実行する場合には地域の設定が必要になるため，`country=JP`を追加している
-  const requestUrl =
-    "https://www.googleapis.com/books/v1/volumes?country=JP&q=intitle:";
-  const result = await requestPromise(`${requestUrl}${keyword}`);
+  // const requestUrl =
+  //   "https://www.googleapis.com/books/v1/volumes?country=JP&q=intitle:";
+  // const result = await requestPromise(`${requestUrl}${keyword}`);
+  // return result;
+
+  const url =
+    "https://www.googleapis.com/books/v1/volumes?country=JP&q=intitle:" +
+    keyword;
+
+  const result = await axios
+    .get(url)
+    .then(function (response) {
+      return response.data;
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+
+  return result;
+};
+
+const getDataFromWeatherApi = async (zip) => {
+  const options = {
+    method: "GET",
+    url: "https://community-open-weather-map.p.rapidapi.com/forecast",
+    params: { zip: zip + ",JP" },
+    headers: {
+      "x-rapidapi-key": "0babf9ac9bmsh1c7c5db9c8b4c8ap188288jsn9dde1990a91f",
+      "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
+    },
+  };
+
+  const result = await axios
+    .request(options)
+    .then(function (response) {
+      return response.data;
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+
   return result;
 };
 
@@ -41,29 +80,13 @@ app.get("/user/:userId", (req, res) => {
 });
 
 app.get("/gbooks/:keyword", cors(), async (req, res) => {
-  const response = await getDataFromApi(req.params.keyword);
+  const response = await getDataFromBookApi(req.params.keyword);
   res.send(response);
 });
 
 app.get("/weather/forecast/:zip", cors(), async (req, res) => {
-  const options = {
-    method: "GET",
-    url: "https://community-open-weather-map.p.rapidapi.com/forecast",
-    params: { zip: req.params.zip + ",JP" },
-    headers: {
-      "x-rapidapi-key": "0babf9ac9bmsh1c7c5db9c8b4c8ap188288jsn9dde1990a91f",
-      "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
-    },
-  };
-
-  axios
-    .request(options)
-    .then(function (response) {
-      res.send(response.data);
-    })
-    .catch(function (error) {
-      console.error(error);
-    });
+  const response = await getDataFromWeatherApi(req.params.zip);
+  res.send(response);
 });
 
 const api = functions.https.onRequest(app);
